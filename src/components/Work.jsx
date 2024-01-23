e6cba7d463140e0766279ab625dadb7df8bf63bd
Ashish Gupta <ash75356@gmail.com>
2024-01-23 16:53:34 +0530

    update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,9 +6,9 @@ import keebook from '../assets/keebook.png'
 const Work = () => {
   return (
     <div name="work" className='w-full pt-[150px] md:h-screen text-gray-300 bg-[#143447]'>
-        <div className='max-w-[1000px] mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300 '>Work</p>
+                <p className='text-4xl font-bold inline border-b-4 border-red-600 text-gray-300 '>Work</p>
                 <p className='py-6'>History</p>
             </div>
 
